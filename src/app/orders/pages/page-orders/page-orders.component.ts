22809032a811bc6d7c3b3baab213f2603812be50
christophe gueroult <christophegueroult@gmail.com>
2020-08-27 13:42:04 +0100

    unsubscribe example

--- src/app/orders/pages/page-orders/page-orders.component.ts
+++ src/app/orders/pages/page-orders/page-orders.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { OrdersService } from '../../services/orders.service';
 import { Order } from 'src/app/shared/models/order';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-page-orders',
   templateUrl: './page-orders.component.html',
   styleUrls: ['./page-orders.component.scss']
 })
-export class PageOrdersComponent implements OnInit {
+export class PageOrdersComponent implements OnInit, OnDestroy {
   public title: string;
   public collection: Order[];
   public headers: string[];
+  private sub: Subscription;
   constructor(
     private os: OrdersService
   ) { }
@@ -26,15 +28,18 @@ export class PageOrdersComponent implements OnInit {
       'State',
     ];
     this.title = 'All orders';
-    this.os.collection.subscribe(
+    this.sub = this.os.collection.subscribe(
       (datas) => {
         this.collection = datas;
-        console.log(this.collection);
       }
     );
   }
 
   public openPopup() {
     console.log('ouvrir une popup');
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
